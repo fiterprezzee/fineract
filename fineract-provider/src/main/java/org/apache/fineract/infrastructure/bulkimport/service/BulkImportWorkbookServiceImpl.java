bb99566e548d694ef8b949c79565608aec4c3e56
Naphlin Akena <starnapho@gmail.com>
2026-04-13 09:54:44 +0300

    FTSP-24: Library version upgrades

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/bulkimport/service/BulkImportWorkbookServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/bulkimport/service/BulkImportWorkbookServiceImpl.java
@@ -49,8 +49,8 @@ import org.apache.fineract.infrastructure.documentmanagement.service.DocumentWri
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.tika.Tika;
-import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MimeTypes;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -93,9 +93,7 @@ public class BulkImportWorkbookServiceImpl implements BulkImportWorkbookService 
                 final byte[] bytes = baos.toByteArray();
                 InputStream clonedInputStream = new ByteArrayInputStream(bytes);
                 final BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(bytes));
-                final Tika tika = new Tika();
-                final TikaInputStream tikaInputStream = TikaInputStream.get(bis);
-                final String fileType = tika.detect(tikaInputStream);
+                final String fileType = MimeTypes.getDefaultMimeTypes().detect(bis, new Metadata()).toString();
                 if (!fileType.contains("msoffice") && !fileType.contains("application/vnd.ms-excel")) {
                     // We had a problem where we tried to upload the downloaded
                     // file from the import options, it was somehow changed the

5f51a64f0994d749bcbf80f355a45967108f5ab9
tabrez-fiter <tabrez@fiter.io>
2026-04-28 20:04:07 +0800

    FTSP-44 : Enable Reporting (Pentaho Integration & Report Activation on Prezi Instance)

--- fineract-core/src/main/java/org/apache/fineract/infrastructure/core/config/FineractProperties.java
+++ fineract-core/src/main/java/org/apache/fineract/infrastructure/core/config/FineractProperties.java
@@ -391,9 +391,18 @@ public class FineractProperties {
 
     @Getter
     @Setter
+    public static class FineractPentahoProperties {
+
+        private String path;
+    }
+
+
+    @Getter
+    @Setter
     public static class FineractReportProperties {
 
         private FineractExportProperties export;
+        private FineractPentahoProperties pentaho;
     }
 
     @Getter

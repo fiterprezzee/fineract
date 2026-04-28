e0641a9a8fd0914ef92a7ad086cf9593c79d760b
tabrez-fiter <tabrez@fiter.io>
2026-04-29 07:45:22 +0800

    FTSP-44 : Enable Reporting (Pentaho Integration & Report Activation on Prezi Instance)

--- fineract-provider/src/main/java/org/apache/fineract/interoperation/handler/DeleteInteropIdentifierHandler.java
+++ fineract-provider/src/main/java/org/apache/fineract/interoperation/handler/DeleteInteropIdentifierHandler.java
@@ -23,6 +23,7 @@ import static org.apache.fineract.interoperation.util.InteropUtil.ENTITY_NAME_ID
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import java.util.List;
+import java.util.Locale;
 import org.apache.fineract.commands.annotation.CommandType;
 import org.apache.fineract.commands.handler.NewCommandSourceHandler;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
@@ -52,7 +53,7 @@ public class DeleteInteropIdentifierHandler implements NewCommandSourceHandler {
         int length = split.size();
         String subIdOrType = Strings.emptyToNull(StringUtils.trimWhitespace(split.get(length - 1)));
         String idValue = split.get(length - 2);
-        InteropIdentifierType idType = InteropIdentifierType.valueOf(split.get(length - 3).toUpperCase());
+        InteropIdentifierType idType = InteropIdentifierType.valueOf(split.get(length - 3).toUpperCase(Locale.ROOT));
         return this.interopService.deleteAccountIdentifier(idType, idValue, subIdOrType);
     }
 }
